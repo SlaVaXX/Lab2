89da384ed8ef329c9de4aff123e66c427f46675f
SlaVaXX <svyatoslav.solianyk@gmail.com>
2023-03-01 13:29:32 +0200

    Lab2

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static int size = 7;//розмір місиву векторів за варіантом
+        static void init(Vector[] vectors)
+        {
+            for (int i = 0,j = 6; j > 3; i++, j--)
+            {
+                vectors[j] = vectors[i];
+            }
+            vectors[3] = new Vector();
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Vector[] vectors = new Vector[size];
+            for (int i = 0;i < 3;)
+            {
+                Console.WriteLine("\nВведіть наступну дію:\n(new - створити новий вектор)\n(change - змінити координати вектора)\n(data - отримати дані про даний вектор)\n(end - завершити виконання програми)");
+                string request = Console.ReadLine();
+                Console.Clear();
+                if (request == "new")
+                {
+                    if (i == 3)
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    Console.WriteLine("Введіть довжину вектора:");
+                    string temp = Console.ReadLine();
+                    int length = Convert.ToInt32(temp);
+                    Console.WriteLine("Введіть кут під яким знаходиться вектор:");
+                    temp = Console.ReadLine();
+                    int angle = Convert.ToInt32(temp);
+                    vectors[i] = new Vector(angle, length);
+                    i++;
+                    continue;
+                }
+                else if (request == "change" && i != 0)
+                {
+                    Console.Write("Введіть координати вектора:\nX = ");
+                    float X = (float)Math.Round(float.Parse(Console.ReadLine()), 2);
+                    Console.Write("\nY = ");
+                    float Y = (float)Math.Round(float.Parse(Console.ReadLine()), 2);
+                    vectors[i].changeCoordinates(new Vector2(X, Y));
+                    Console.Clear();
+                    vectors[i].vectorData();
+                    continue;
+                }
+                else if (request == "data" && i != 0)
+                {
+                    vectors[i].vectorData();
+                    continue;
+                }
+                else if (request == "end")
+                {
+                    Console.Clear();
+                    break;
+                }
+            }
+            init(vectors);
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Vector
+    {
+        private double angle;
+        private double length;
+        private Vector2 coordinates;
+        public Vector(double angle, double length)
+        {
+            this.angle = angle;
+            if (length < 0)
+                length = Math.Abs(length);
+            this.length = length;
+            coordinates = setCoordinates(angle, length);
+        }
+        public Vector()
+        {
+            
+        }
+        public Vector2 setCoordinates(double angle, double length)//обчислення координат вектора на основі його довжини та кута
+        {
+            double x = Math.Round(length * Math.Cos(angle), 2);
+            double y = Math.Round(length * Math.Sin(angle), 2);
+            Vector2 result = new Vector2((float)x, (float)y);
+            return result;
+        }
+        public Vector2 getCoordinates
+        {
+            get { return coordinates; }
+        }
+        public void vectorData ()
+        {
+            Console.WriteLine($"\nКоординати вектора: {coordinates}\nКут вектора: {angle}");
+        }
+        public void changeCoordinates(Vector2 newCoordinates)
+        {
+            coordinates.X = newCoordinates.X;
+            coordinates.Y = newCoordinates.Y;
+        }
+        ~Vector()
+        {
+            Console.WriteLine("The end");
+        }
+    }
+}
